728648ee56663c76e2faf14e0c6ec730d8af7d85
Nikhil Sharma <nikhil.harsh.sharma@gmail.com>
2022-06-30 04:18:23 +0530

    feat: context

---
+++ src/context/QuizContext.js
@@ -0,0 +1,21 @@
+import { useContext, createContext, useReducer } from 'react';
+import { QuizReducer } from '../reducers';
+const QuizContent = createContext();
+
+const useQuiz = () => useContext(QuizContent);
+
+const QuizProvider = ({ children }) => {
+  const [quizState, quizDispatch] = useReducer(QuizReducer, {
+    categories: [],
+    difficulty: '',
+    quantity: '',
+    result: {},
+  });
+  return (
+    <QuizContent.Provider value={{ quizState, quizDispatch }}>
+      {children}
+    </QuizContent.Provider>
+  );
+};
+
+export { QuizProvider, useQuiz };
